f74f1e4f4573cd578c5a425422123cca4edea716
jalva <pjalva90@gmail.com>
2025-02-22 11:31:39 +0530

    project initialize

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const  app=express()
+const path=require("path")
+const env=require("dotenv").config()
+const db=require("./config/db")
+const userRouter=require("./routes/userRouter")
+db()
+
+const PORT=process.env.PORT||3000
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","ejs")
+app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname,"public")))
+
+app.use("/",userRouter)
+console.log("reach");
+
+
+app.listen(PORT,()=>{
+    console.log(`running on http://localhost:${PORT}`)
+})
+
+module.exports=app
