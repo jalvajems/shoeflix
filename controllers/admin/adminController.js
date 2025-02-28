41cde67037e4cd3b23d880bf343be51c9d99c47d
jalva <pjalva90@gmail.com>
2025-02-28 20:47:16 +0530

    dash board added

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -2,6 +2,10 @@ const User=require("../../models/userSchema")
 const mongoose=require("mongoose")
 const bcrypt=require("bcrypt")
 
+const pageerror=async(req,res)=>{
+    res.render("admin-error")
+}
+
 
 const loadLogin=(req,res)=>{
     if(req.session.admin){
@@ -48,5 +52,6 @@ const loadDashboard=async(req,res)=>{
 module.exports={
     loadLogin,
     login,
-    loadDashboard
+    loadDashboard,
+    pageerror
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const router=express.Router();
 const adminController=require("../controllers/admin/adminController")
+const {userAuth,adminAuth}=require("../middlewares/auth")
 
+
+router.get("/pageerror",adminController.pageerror)
 router.get('/login',adminController.loadLogin)
 router.post('/login',adminController.login)
-router.get('/dashboard',adminController.loadDashboard)
+router.get('/dashboard',adminAuth,adminController.loadDashboard)
 
 module.exports=router
